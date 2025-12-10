9741a78cea39fd36241fc2f7027bef7091b31af1
Crypoune <a.messenet@gmail.com>
2025-12-10 15:27:43 +0100

    feat: implement binary_tree_sibling function

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,5 +10,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	return ((node == node->parent->left) ? node->parent->right : node->parent->left);
+	if (node->parent->left && node->parent->left != node)
+		return (node->parent->left);
+
+	if (node->parent->right && node->parent->right != node)
+		return (node->parent->right);
+
+	return (NULL);
 }
