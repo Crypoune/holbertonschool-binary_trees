ae0a0d70e92e766b0a45bb07de2765de772af113
Crypoune <a.messenet@gmail.com>
2025-12-10 15:35:18 +0100

    feat: implement 18-binary_tree_uncle function

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	return ((node == node->parent->left) ?
+		node->parent->right : node->parent->left);
+}
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node to find the uncle
+ * Return: Pointer to the uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
